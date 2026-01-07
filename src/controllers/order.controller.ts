01351e314bd211d0452d023f1f68e53229b303d5
lirhzaaa <mhmdazhrilnmldn@gmail.com>
2026-01-07 23:24:21 +0700

    feat: add API Order Complete

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,9 +1,14 @@
 import { Response } from "express";
 import { IReqUser } from "../utils/interfaces";
 import response from "../utils/response";
-import OrderModel, { orderDAO, TypeOrder } from "../models/orderModels";
+import OrderModel, {
+  orderDAO,
+  OrderStatus,
+  TypeOrder,
+  TypeVoucher,
+} from "../models/orderModels";
 import TicketModel from "../models/ticketModels";
-import { QueryFilter } from "mongoose";
+import { getId } from "../utils/id";
 
 export default {
   async create(req: IReqUser, res: Response) {
@@ -88,9 +93,71 @@ export default {
 
   async findAllMember(req: IReqUser, res: Response) {},
 
-  async completed(req: IReqUser, res: Response) {},
+  async completed(req: IReqUser, res: Response) {
+    try {
+      const { orderId } = req.params;
+      const userId = req.user?.id;
 
-  async pending(req: IReqUser, res: Response) {},
+      const order = await OrderModel.findOne({
+        orderId,
+        createdBy: userId,
+      });
 
-  async calcelled(req: IReqUser, res: Response) {},
+      if (!order) return response.notFound(res, "Order Not Found");
+      if (order.status === OrderStatus.COMPLETED)
+        return response.error(res, null, "you have been completed this order");
+
+      const vouchers: TypeVoucher[] = Array.from(
+        { length: order.quantity },
+        () => {
+          return {
+            isPrint: false,
+            voucherId: getId(),
+          } as TypeVoucher;
+        }
+      );
+
+      const result = await OrderModel.findOneAndUpdate(
+        {
+          orderId,
+          createdBy: userId,
+        },
+        {
+          vouchers,
+          status: OrderStatus.COMPLETED,
+        },
+        {
+          new: true,
+        }
+      );
+
+      const ticket = await TicketModel.findById(order.ticket)
+
+      if (!ticket) return response.notFound(res, "Ticket and Order Not Found")
+    
+        await TicketModel.updateOne({
+            _id: ticket._id
+        }, {
+            quantity: ticket.quantity - order.quantity
+        })
+
+      response.success(res, result, "Success to complete an order")
+    } catch (error) {
+      response.error(res, error, "Failed to completed an order");
+    }
+  },
+
+  async pending(req: IReqUser, res: Response) {
+    try {
+    } catch (error) {
+      response.error(res, error, "Failed to pending an order");
+    }
+  },
+
+  async cancelled(req: IReqUser, res: Response) {
+    try {
+    } catch (error) {
+      response.error(res, error, "Failed to cancelled an order");
+    }
+  },
 };
