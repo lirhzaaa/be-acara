9579aa1b58a582daf7158b7ef66dcaba8a8b7cc7
lirhzaaa <mhmdazhrilnmldn@gmail.com>
2026-01-07 17:55:51 +0700

    feat: create setup payment gateway

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,8 @@
 export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || "";
 export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
 export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || "";
+
+
+export const MIDTRANS_CLIENT_KEY: string = process.env.MIDTRANS_CLIENT_KEY || ""
+export const MIDTRANS_SERVER_KEY: string = process.env.MIDTRANS_SERVER_KEY || ""
+export const MIDTRANS_TRANSACTIN_URL: string = process.env.MIDTRANS_TRANSACTIN_URL || ""
