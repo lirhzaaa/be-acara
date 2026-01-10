dc01336ee2df3e2a1c836d1adeba61d6282bee68
lirhzaaa <mhmdazhrilnmldn@gmail.com>
2026-01-10 20:10:02 +0700

    feat: fix bug code orders

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -1,6 +1,6 @@
-import { customAlphabet } from "nanoid";
+import { customAlphabet } from "nanoid/non-secure";
 
 export const getId = (): string => {
-  const nanoid = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", );
-  return nanoid(5);
+  const nanoid = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 5);
+  return nanoid();
 };
